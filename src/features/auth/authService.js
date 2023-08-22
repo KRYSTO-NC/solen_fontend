6ca776e52fb349733925e053cfd79e0783971333
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2023-08-22 13:13:22 +1100

    initial commit

---
+++ src/features/auth/authService.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const API_URL = process.env.REACT_APP_BASE_API_URL + '/auth'
+
+// login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + '/login', userData)
+
+  if (response.data) {
+    localStorage.setItem('userToken', JSON.stringify(response.data.token))
+
+  }
+  return response.data
+}
+
+// get current logged user
+const getCurrentUser = async (userId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  console.log(config)
+  const response = await axios.get(`${API_URL}/auth/me`, config)
+  return response.data
+}
+
+
+
+// Logout
+const logout = () => {
+  localStorage.removeItem('userToken')
+}
+
+const authService = {
+  logout,
+  login,
+  getCurrentUser
+}
+
+export default authService
